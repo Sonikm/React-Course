6abed9b7e5d90812657de9466ffbd99788aad323
Soni <sonikm443@gmail.com>
2023-07-24 15:39:44 +0530

    Added rating functionality

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import StarRating from './StarRating';
+
+function Test() {
+  const [moiveRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color='blue'  maxRating={10} size={28} onSetMovieRating={setMovieRating} />
+      <p>This movie was rated {moiveRating} stars</p>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} size={48} color={"#fcc419"} className={"test"} messages={["Terrible","Bad", "Okay", "Good", "Amazing"]} defaultRating={3}/>
+    <Test/>
+    {/* <StarRating size={28} color={"red"} />
+    <StarRating size={18} color={"green"} /> */}
   </React.StrictMode>
 );
 
